ce5ccd6b2973b2b2fffd9cc1d8554a5502c8951c
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-05-05 21:30:57 +0600

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { Outlet } from "react-router-dom";
 import Header from "./layout/Header";
 import Footer from "./layout/Footer";
+import Loading from "./components/loading/Loading";
+import { useEffect, useState } from "react";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
+
+  if (loading) return <Loading />;
   return (
     <div className="bg-white text-black flex flex-col justify-between min-h-[100vh] min-w-[100vw] overflow-x-hidden">
       <div className="header">
