1811d764abaf9863a4f9a2d30fef69a4b86e8ca3
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-05-05 21:33:00 +0600

    profile ...

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="px-[17%] py-[2%]">
+      <div className="flex items-center">
+        <img
+          src="https://i.ibb.co.com/yQGvpKV/user-icon-on-transparent-background-free-png.webp"
+          className="w-30 h-auto"
+          alt=""
+        />
+        <div className="">
+            <h1 className="text-3xl font-semibold">Abdur Rahman</h1>
+            <p>abdurrahmanass2024@gmail.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
