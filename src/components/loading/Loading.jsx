a6eb688ba98e3efcc805c782184c66dbb59b3477
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-05-05 21:30:13 +0600

    loading

---
+++ src/components/loading/Loading.jsx
@@ -0,0 +1,10 @@
+import "./loading.css";
+const Loading = () => {
+  return (
+    <div className="h-[100vh] w-[100vw] bg-white fixed flex justify-center items-center top-0 left-0">
+      <div class="loader"></div>
+    </div>
+  );
+};
+
+export default Loading;
