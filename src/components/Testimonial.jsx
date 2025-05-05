912ad74038d1e06519b4ea44f60b6a3d2888d7dd
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-05-05 21:31:36 +0600

    timonial

---
+++ src/components/Testimonial.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Testimonials } from "./ui/testimonials";
+
+const Testimonial = () => {
+  return (
+    <div className="px-[5%] lg:px-[18%] overflow-x-hidden">
+      <Testimonials />
+    </div>
+  );
+};
+
+export default Testimonial;
