a8ff9066093d756e99f7960a0cbd16b4ef635e29
Abdur Rahman <152638668+abdurrahman482937@users.noreply.github.com>
2025-05-05 21:30:35 +0600

    private

--- src/private/Private.jsx
+++ src/private/Private.jsx
@@ -1,22 +1,17 @@
 import { useContext } from "react";
 import { Navigate } from "react-router-dom";
-import { authContext } from "../Auth/Authprovider";
+import { authContext } from "../context/AuthProvider";
+import Loading from "../components/loading/Loading";
 
 const Private = ({ children }) => {
   const { user, loading } = useContext(authContext);
   if (loading) {
-    return (
-      <div className="bg-gray-800 min-h-[550px] flex items-center justify-center space-x-2">
-        <div className="w-4 h-4 rounded-full animate-pulse bg-violet-400 dark:bg-violet-600"></div>
-        <div className="w-4 h-4 rounded-full animate-pulse bg-violet-400 dark:bg-violet-600"></div>
-        <div className="w-4 h-4 rounded-full animate-pulse bg-violet-400 dark:bg-violet-600"></div>
-      </div>
-    );
+    return <Loading />;
   }
   if (user) {
     return children;
   }
-  return <Navigate to="/signin"></Navigate>;
+  return <Navigate to="/"></Navigate>;
 };
 
 export default Private;
